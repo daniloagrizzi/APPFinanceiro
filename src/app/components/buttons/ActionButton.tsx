bbceb1732f6ed886e5b639249ce8abc253ba1f43
square.ggm <square.ggm@gmail.com>
2025-04-24 23:38:35 +0000

    Progresso de criação de componente ActionButton

--- src/app/components/buttons/ActionButton.tsx
+++ src/app/components/buttons/ActionButton.tsx
@@ -5,19 +5,22 @@ import { ButtonProps } from "@/app/types/ButtonProps";
 
 const ActionButton: React.FC<ButtonProps> = ({type,className,text ='label',onClick,icon='/wo-axolot.png'}) => {
 
+    //Icon Styles
+const IconStyle = 'w-[24px] h-[24px] object-left m-[5px]' // Default Style for all Devices
 
-const SMicon = 'w-[24px] h-[24px] object-left m-[5px]'
-const SMstyles ='h-[32px] w-[80px] bg-dark-purple rounded-[0.625rem] flex items-center hover:bg-light-purple'
+const ButtonSizes = 'sm:h-[32px] sm:w-[80px] md:h-[36px] md:w-[100px] lg:h-[40px] lg:w-[120px] xl:w-[44px] xl:w-[140px] 2xl:h-[48px] 2xl:w-[160px]  rounded-[0.625rem]'
+const ButtonStyle =`flex bg-dark-purple hover:bg-light-purple ${ButtonSizes}`
+
 
 return(
 
 <>
-<button className={SMstyles}>
+<button className={ButtonStyle}>
 <div>
-<img src={icon} alt="Action button icon" className={SMicon} />
+<img src={icon} alt="Action button icon" className={IconStyle} />
 </div>
-<div className="m-[5px]">
-{text}
+<div className="flex items-center m-[20px]">
+    <label>{text}</label>
 </div>
 </button>
 </>
